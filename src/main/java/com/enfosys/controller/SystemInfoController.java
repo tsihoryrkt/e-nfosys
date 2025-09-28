d29bbcd37b0cc9bb389b82ad8558476e2cacd8be
Tsihory Niaina RAKOTOSON <tsihoryrkt@gmail.com>
2025-09-28 23:56:05 +0300

    delete racine controller

--- src/main/java/com/enfosys/controller/SystemInfoController.java
+++ src/main/java/com/enfosys/controller/SystemInfoController.java
@@ -5,8 +5,4 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SystemInfoController {
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to E-nfosys!";
-    }
 }
