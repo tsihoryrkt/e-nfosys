32ed77cc4ae0e7008c325d1e31bc04af628a1c7a
Tsihory Niaina RAKOTOSON <tsihoryrkt@gmail.com>
2025-11-16 16:40:05 +0300

    feat(fetch): map ip address

--- src/main/resources/static/js/system-info.js
+++ src/main/resources/static/js/system-info.js
@@ -2,5 +2,6 @@ fetch('/system-info')
     .then(response => response.json())
     .then(data => {
         document.getElementById('hostname').textContent = data.hostname;
+        document.getElementById('ip').textContent = data.ip;
     })
     .catch(error => console.error('Error :', error));
