5ca9a45376dea3cff015e22569f8f8859253835b
Tsihory Niaina RAKOTOSON <tsihoryrkt@gmail.com>
2025-11-16 11:07:30 +0300

    feat(fetch): fetch hostname

---
+++ src/main/resources/static/js/system-info.js
@@ -0,0 +1,6 @@
+fetch('/system-info')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('hostname').textContent = data.hostname;
+    })
+    .catch(error => console.error('Error :', error));
