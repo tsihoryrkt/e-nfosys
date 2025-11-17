6c492a7c851b48ea467e9f1f1dac89af461acdf2
Tsihory Niaina RAKOTOSON <tsihoryrkt@gmail.com>
2025-11-17 23:21:47 +0300

    feat(js): fetch and display Docker environment status

--- src/main/resources/static/js/system-info.js
+++ src/main/resources/static/js/system-info.js
@@ -1,7 +1,25 @@
 fetch('/system-info')
     .then(response => response.json())
     .then(data => {
+
+        // Hostname & IP
         document.getElementById('hostname').textContent = data.hostname;
         document.getElementById('ip').textContent = data.ip;
+
+        // Docker status
+        const box = document.getElementById("docker-status-box");
+        const text = document.getElementById("docker-status-text");
+        const desc = document.getElementById("docker-description");
+
+        if (data.docker === true) {
+            box.classList.add("border-green-100", "bg-green-50", "text-green-600");
+            text.textContent = "Running";
+            desc.textContent = "Running inside Docker container.";
+        } else {
+            box.classList.add("border-red-100", "bg-red-50", "text-red-600");
+            text.textContent = "Not Running";
+            desc.textContent = "Application is not running inside Docker.";
+        }
+
     })
     .catch(error => console.error('Error :', error));
