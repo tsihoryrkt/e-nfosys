aef6c027632450d890cd52d7abecaf296b6d705e
Tsihory Niaina RAKOTOSON <tsihoryrkt@gmail.com>
2025-11-18 23:35:46 +0300

    feat(js): fetch and display kubernetes environment status

--- src/main/resources/static/js/system-info.js
+++ src/main/resources/static/js/system-info.js
@@ -6,7 +6,9 @@ fetch('/system-info')
         document.getElementById('hostname').textContent = data.hostname;
         document.getElementById('ip').textContent = data.ip;
 
-        // Docker status
+        /* ---------------------------
+           Docker status
+        --------------------------- */
         const box = document.getElementById("docker-status-box");
         const text = document.getElementById("docker-status-text");
         const desc = document.getElementById("docker-description");
@@ -19,6 +21,23 @@ fetch('/system-info')
             box.classList.add("border-red-100", "bg-red-50", "text-red-600");
             text.textContent = "Not Running";
             desc.textContent = "Application is not running inside Docker.";
+        }
+
+        /* ---------------------------
+           Kubernetes status
+        --------------------------- */
+        const k8sBox = document.getElementById("k8s-status-box");
+        const k8sText = document.getElementById("k8s-status-text");
+        const k8sDesc = document.getElementById("k8s-description");
+
+        if (data.kubernetes === true) {
+            k8sBox.classList.add("border-green-100", "bg-green-50", "text-green-600");
+            k8sText.textContent = "Running";
+            k8sDesc.textContent = "Running inside a Kubernetes pod.";
+        } else {
+            k8sBox.classList.add("border-red-100", "bg-red-50", "text-red-600");
+            k8sText.textContent = "Not Running";
+            k8sDesc.textContent = "Application is not running inside Kubernetes.";
         }
 
     })
